253af2b180f99b356525aedf7045dfe068ba9694
nadeem099 <ahmedinadeem99@gmail.com>
2022-02-01 15:10:46 +0530

    Updated minMax.js with comments and updated readme.md

--- Array/minMax.js
+++ Array/minMax.js
@@ -2,6 +2,7 @@ function getMinMax(arr, n) {
   //code here
   let min;
   let max;
+  //compare first two elements of array and assign min and max based on that
   if (arr[0] > arr[1]) {
     max = arr[0];
     min = arr[1];
@@ -10,6 +11,7 @@ function getMinMax(arr, n) {
     min = arr[0];
   }
 
+  //compare each element with min and max and change the values accordingly
   for (let i = 2; i < arr.length; i++) {
     if (arr[i] < min) {
       min = arr[i];
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# js-7dwg4u
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-7dwg4u)
+Turns out even if you do a lot of projects and have experience you still need DSA in interviews so this is my DSA journey.
